c0269d374b7fb5bb155bcb6334b44be8af2349b6
ghazanfarmirza <ghazanfar.mirza97@gmail.com>
2022-07-04 16:40:44 +0500

    integrated strapi-api

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,14 +5,51 @@ import { classnames } from "../utils/general";
 import OutputWindow from "./OutputWindow";
 import CustomInput from "./CustomInput";
 import OutputDetails from "./OutputDetails";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
-const javascriptDefault = `/** Problem: xyz: problem statement.**/`;
+const javascriptDefault = "";
 
 const Landing = () => {
   const [code, setCode] = useState(javascriptDefault);
   const [customInput, setCustomInput] = useState("");
   const [outputDetails, setOutputDetails] = useState(null);
   const [processing, setProcessing] = useState(null);
+  const [markdownProblem, setMarkdownProblem] = useState("");
+  const markdown = `
+  **Sample Code**
+
+  # code block
+  print '3 backticks or'
+  print 'indent 4 spaces'
+
+  # A demo of 'react-markdown'
+
+  A paragraph with *emphasis* and **strong importance**.
+
+  > A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+  # A demo of 'remark-gfm' (GitHub Flavor Markdown) with 'react-markdown' and 'remark-gfm' plugins installed in 'package.json' file. 
+  
+  A table:
+
+  | a | b |
+  | - | - |
+
+  A new table: 
+
+  | a | b |
+  | - | - |
+
+  `;
+
+  useEffect(() => {
+    axios.get("http://localhost:1337/api/problems").then((response) => {
+      console.log(response.data.data[0].attributes.CodingProblem);
+      setMarkdownProblem(response.data.data[0].attributes.CodingProblem);
+      console.log(response);
+    });
+  }, []);
 
   const onChange = (action, data) => {
     switch (action) {
@@ -99,12 +136,31 @@ const Landing = () => {
   return (
     <>
       <div className="flex flex-row space-x-4 items-center flex-wrap p-14">
+        <div className="flex flex-col basis-1/4 flex-1 border-8 border-black h-80">
+          <h1 className="text-2xl font-bold">Coding Problem</h1>
+          <span className="text-gray-600">{markdownProblem}</span>
+          <br />
+          <ReactMarkdown
+            children={markdownProblem}
+            remarkPlugins={[remarkGfm]}
+          />
+        </div>
         <div className="flex flex-col items-center basis-1/2 flex-1 border-8 border-black">
           <CodeEditorWindow
             code={code}
             onChange={onChange}
             language="javascript"
           />
+          <button
+            onClick={handleCompile}
+            disabled={!code}
+            className={classnames(
+              "mt-4 border-2 border-black z-10 rounded-md items-center w-full px-4 py-2 transition bg-green-500 hover:bg-green-700 hover:text-white",
+              !code ? "opacity-50" : ""
+            )}
+          >
+            {processing ? "Processing..." : "Compile and Execute"}
+          </button>
         </div>
 
         <div className="right-container flex flex-shrink-0 w-[30%] flex-col basis-1/4 flex-1">
@@ -114,16 +170,6 @@ const Landing = () => {
               customInput={customInput}
               setCustomInput={setCustomInput}
             />
-            <button
-              onClick={handleCompile}
-              disabled={!code}
-              className={classnames(
-                "mt-4 border-2 border-black z-10 rounded-md items-center w-full px-4 py-2 transition bg-green-500 hover:bg-green-700 hover:text-white",
-                !code ? "opacity-50" : ""
-              )}
-            >
-              {processing ? "Processing..." : "Compile and Execute"}
-            </button>
           </div>
           {outputDetails && <OutputDetails outputDetails={outputDetails} />}
         </div>
